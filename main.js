2b3421ec0714334f92fa61f4d4a19536797fe093
Agradip Jana <92352889+AgradipJana@users.noreply.github.com>
2023-09-29 14:53:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          customgreen: 'rgb(0, 255, 162)',
+          customblack: '#00000046',
+          customblackhover: '#000000',
+        }
+      }
+    }
+  }
+  // background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%);
+  // -webkit-background-clip: text;
+  //         /* Also define standard property for compatibility */
+  //         background-clip: text;
+  // -webkit-text-fill-color: transparent;
+  
+  // font-size: 200px; /* change this to change the size*/
